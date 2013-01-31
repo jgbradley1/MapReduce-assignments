cf668b1eca5299f6cd86813ea97948f56d77a517
Josh Bradley <jgbradley1@gmail.com>
2013-01-31 07:13:59 -0500

    another attempt

--- assignment1/run-assignment1.sh
+++ assignment1/run-assignment1.sh
@@ -3,6 +3,9 @@
 
 echo "Starting MapReduce job...\n"
 
+#unpack the Cloud9 directory
+tar -xvf src/Cloud9.tgz -C src/
+
 # execute the MapReduce job
 src/Cloud9/etc/hadoop-cluster.sh edu.umd.cloud9.example.simple.DemoWordCount -input bible+shakes.nopunc.gz -output jgbradley1 -numReducers 5
 

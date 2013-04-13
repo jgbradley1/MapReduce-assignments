1f49fc0ed217cdeb29d2c6be7f4eec20cf1dc8b5
Josh Bradley <jgbradley1@gmail.com>
2013-04-13 03:28:57 -0400

    Fixed indentation to be coherent with rest of project.

--- assignment6/src/test/SampleTest.java
+++ assignment6/src/test/SampleTest.java
@@ -4,9 +4,9 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class SampleTest {
-  @Test
-  public void test1() throws Exception {
-    assertEquals(1, Integer.parseInt("1"));
-    assertTrue(1 < 2);
-  }
+    @Test
+    public void test1() throws Exception {
+        assertEquals(1, Integer.parseInt("1"));
+        assertTrue(1 < 2);
+    }
 }
